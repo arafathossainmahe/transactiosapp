c7b360ef7f0392702731a8f664590d73b2a4ea9a
arafathossainmahe <54477175+arafathossainmahe@users.noreply.github.com>
2021-06-19 21:39:55 +0600

    home1

--- lib/home.dart
+++ lib/home.dart
@@ -10,100 +10,271 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.blue[50],
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Container(
-                height: 300,
-                width: 350,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Container(
+                  height: 300,
+                  width: 350,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(blurRadius: 5),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 35),
+                        child: Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(50),
+                            boxShadow: [
+                              BoxShadow(blurRadius: 5),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Hira Riyaz',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text('Flutter Developer'),
+                      SizedBox(height: 10,),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30,right: 30),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  'Tk 9000',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                                Text('Income'),
+                              ],
+                            ),
+                            
+                            Column(
+                              children: [
+                                Text(
+                                  'Tk 9000',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                                Text('Income'),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  'Tk 9000',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                                Text('Income'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-              child: ListTile(
-                title: Text(
-                  'Overview',
-                  style: TextStyle(fontSize: 25),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                child: ListTile(
+                  title: Text(
+                    'Overview',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  //leading: Icon(Icons.notifications_off_outlined),
+                  trailing: Text('sep,13,2021'),
                 ),
-                //leading: Icon(Icons.notifications_off_outlined),
-                trailing: Text('sep,13,2021'),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-              child: ListTile(
-                leading: Container(
-                  height: 40,
-                  width: 40,
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                child: Container(
+                  height: 70,
+                  width: size.width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(blurRadius: 5),
+                      ]),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Container(
+                          height: 55,
+                          width: 55,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blue[50],
+                          ),
+                          child: Icon(
+                            Icons.arrow_upward,
+                            size: 40,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15, top: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Sent',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Text('Sending Paymet to Client'),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 50),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '150',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                child: Container(
+                  height: 70,
+                  width: size.width,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                     color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(blurRadius: 5),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Container(
+                          height: 55,
+                          width: 55,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blue[50],
+                          ),
+                          child: Icon(
+                            Icons.arrow_downward,
+                            size: 40,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15, top: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Sent',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Text('Sending Paymet to Client'),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 50),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '150',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                title: Text(
-                  'Sent',
-                  style: TextStyle(fontSize: 20),
-                ),
-                subtitle: Text('Sending Payment to Client'),
-                trailing: Text(
-                  "150",
-                  style: TextStyle(fontSize: 20),
-                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-              child: ListTile(
-                leading: Container(
-                  height: 40,
-                  width: 40,
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                child: Container(
+                  height: 70,
+                  width: size.width,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(blurRadius: 5),
+                      ]),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Container(
+                          height: 55,
+                          width: 55,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blue[50],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15, top: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Sent',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Text('Sending Paymet to Client'),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 50),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '150',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                title: Text(
-                  'Sent',
-                  style: TextStyle(fontSize: 20),
-                ),
-                subtitle: Text('Sending Payment to Client'),
-                trailing: Text(
-                  "150",
-                  style: TextStyle(fontSize: 20),
-                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-              child: ListTile(
-                leading: Container(
-                  height: 40,
-                  width: 40,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                  ),
-                ),
-                title: Text(
-                  'Sent',
-                  style: TextStyle(fontSize: 20),
-                ),
-                subtitle: Text('Sending Payment to Client'),
-                trailing: Text(
-                  "150",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
